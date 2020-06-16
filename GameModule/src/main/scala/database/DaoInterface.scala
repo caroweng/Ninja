2a47c9281fb0e9038ac34189923a8cb6ff3b1e1e
caroweng <caro.weng@gmx.net>
2020-06-16 13:55:36 +0200

    removed player database stuff

--- GameModule/src/main/scala/database/DaoInterface.scala
+++ GameModule/src/main/scala/database/DaoInterface.scala
@@ -1,14 +1,7 @@
 
 package database
 
-import model.DeskInterface
-import model.component.PlayerInterface
-
 trait DaoInterface {
-
-    def putPlayerInDb(desk: DeskInterface): Option[DeskInterface]
-
-    def getPlayerFromDb(): Option[PlayerInterface]
 
     def putGameInDb(desk: String): Boolean
 
--- GameModule/src/main/scala/database/relational/RelationalDb.scala
+++ GameModule/src/main/scala/database/relational/RelationalDb.scala
@@ -1,35 +1,9 @@
 package database.relational
 
 import database.DaoInterface
-import model.DeskInterface
-import model.component.{Desk, PlayerInterface}
 
 object RelationalDb extends DaoInterface {
-    private val playerMapping: PlayerMapping.type = PlayerMapping
     private val gameMapping: GameMapping.type = GameMapping
-
-    def putPlayerInDb(desk: DeskInterface): Option[DeskInterface] = {
-        try {
-            val tryPlayer = playerMapping.putPlayerInDb(desk.player1)
-            if(tryPlayer) {
-                println("Player was saved in database.")
-                Some(desk)
-            } else {
-                println("Error: Player could not be saved in database.")
-                None
-            }
-        } catch {
-            case _: Throwable => None
-        }
-    }
-
-    def getPlayerFromDb(): Option[PlayerInterface] = {
-        try {
-            playerMapping.getPlayerFromDb()
-        } catch {
-            case _: Throwable => None
-        }
-    }
 
     override def putGameInDb(desk: String): Boolean = {
         try {
